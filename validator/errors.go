8cfaf4b07f7db5e86bfb9dfb0bf6d3b12a83db69
Saman <sezarsaman@gmail.com>
2025-11-30 17:37:44 +0330

    error messages to farsi

--- validator/errors.go
+++ validator/errors.go
@@ -3,11 +3,11 @@ package validator
 import "errors"
 
 var (
-	ErrInvalidLength        = errors.New("national id length must be 10 digits")
-	ErrInvalidCharacter     = errors.New("national id must contain only numeric characters")
-	ErrRepeatedDigits       = errors.New("national id cannot be all repeated digits")
-	ErrInvalidChecksum      = errors.New("national id checksum is invalid")
-	ErrInvalidIBANLength    = errors.New("IBAN length is invalid")
-	ErrInvalidIBANCharacter = errors.New("IBAN contains invalid characters")
-	ErrInvalidIBANChecksum  = errors.New("IBAN checksum is invalid")
+	ErrInvalidLength        = errors.New("کپ ملی باید ۱۰ رقم باشد")
+	ErrInvalidCharacter     = errors.New("کد ملی باید فقط شامل ارقام باشد")
+	ErrRepeatedDigits       = errors.New("کد ملی نمی‌تواند شامل ارقام تکراری باشد")
+	ErrInvalidChecksum      = errors.New("کد ملی نامعتبر است")
+	ErrInvalidIBANLength    = errors.New("طول IBAN نامعتبر است")
+	ErrInvalidIBANCharacter = errors.New("IBAN شامل کاراکترهای نامعتبر است")
+	ErrInvalidIBANChecksum  = errors.New("چک‌سام IBAN نامعتبر است")
 )
